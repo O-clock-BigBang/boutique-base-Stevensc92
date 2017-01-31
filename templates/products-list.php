0d1e45b0445f74a506cb1b02e21f02e0a50a4553
Stevens Costa <stevensc92@gmail.com>
2017-01-31 13:43:21 +0100

    add base html

--- templates/products-list.php
+++ templates/products-list.php
@@ -1,3 +1,23 @@
 		<main>
-			<?php var_dump($products); ?>
+			<hr/><hr/>
+			<?php foreach($products as $product): ?>
+			<article class="product">
+				<div class="product-title"><?= $product['title']; ?></div>
+				<div class="product-img">
+					<img src="images/<?= $product['thumbnail']; ?>" alt="<?= $product['title']; ?>" />
+				</div>
+				<p class="product-short-desc">
+					<?= $product['short_desc']; ?>
+				</p>
+				<div class="product-price">
+					<?= number_format($product['price'], 2); ?> &euro;
+					<?= number_format($product['promo_price'], 2); ?> &euro;
+				</div>
+				<div class="product-more">
+					<a href="">Voir le produit</a>
+					<button>Ajouter au panier</button>
+				</div>
+			</article>
+			<hr/>
+			<?php endforeach; ?>
 		</main>
