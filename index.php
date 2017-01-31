f07831471f4b8317d4663f9894f013abb3973317
Stevens Costa <stevensc92@gmail.com>
2017-01-31 13:05:26 +0100

    add templates, style and reset css

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
+// on require le fichier car on a besoin des données
+require('inc/data.php');
 
+include_once('templates/header.php');
+?>
+		<main>
+
+		</main>
+<?php
+include_once('templates/footer.php');
 ?>
---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/app.css" />
+		<title>Ma petite boutique de chaussons</title>
+	</head>
+	<body>
+		<header>
+			<h1>Chaussons disponibles</h1>
+		</header>
